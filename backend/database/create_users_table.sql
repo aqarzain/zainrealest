f96671c11869c6a193c1d8576e9b3c9fbca2ed49
aqarzain <aqarzain2@gmail.com>
2025-05-15 19:44:33 +0300

    create_users_table.sql

--- backend/database/create_users_table.sql
+++ backend/database/create_users_table.sql
@@ -1,11 +1,10 @@
--- backend/database/create_users_table.sql
-
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
     phone VARCHAR(20),
-    password VARCHAR(255) NOT NULL,
-    role VARCHAR(50) DEFAULT 'user', -- ممكن تكون admin أو user
+    password TEXT NOT NULL,
+    role VARCHAR(50) DEFAULT 'agent',
+    notes TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
